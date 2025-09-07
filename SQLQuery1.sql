51563af091352b51ffb449fa75696abf42ed6752
yousefhanieh8-png <yousefhanieh8@gmail.com>
2025-09-07 13:08:02 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,20 @@
+--kips
+
+select sum(total_revenue) as [Total Revenue]
+from Services_data
+-------------------------------
+select sum(hours) as [Total hours]
+from Services_data
+-------------------------------
+select department,sum(total_revenue) as [Total Revenue]
+from Services_data
+Group by department
+-------------------------------
+select client_name,sum(total_revenue) as [Total Revenue]
+from Services_data
+Group by client_name
+-------------------------------
+select region,sum(total_revenue) as [Total Revenue]
+from Services_data s,Branch_data b
+Where s.Branch_ID=b.Branch_ID
+group by region
